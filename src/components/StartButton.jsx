8cc5c95fcc99acbae13d68887e2e8bc8471f2bad
Andres M <marulandaandres19@gmail.com>
2024-01-23 19:44:08 -0500

    style HomeScreen

---
+++ src/components/StartButton.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+
+const StartButton = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: '#EAEDED',
+    fontSize: 24,
+    textAlign: 'center',
+    fontWeight: '500',
+  },
+});
+export default StartButton;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import StartButton from '../components/StartButton';
 
 const HomeScreen = ({ navigation }) => {
   const navigateToSearch = () => {
@@ -7,31 +8,59 @@ const HomeScreen = ({ navigation }) => {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-      }}
+    <ImageBackground
+      source={require('../assets/pexels.jpg')}
+      style={styles.backgroundImage}
     >
-      <Text style={{ marginBottom: 20, textAlign: 'center' }}>
-        Bienvenido a la Aplicación GitHub Users
-      </Text>
-      <Text>
-        Esta aplicación te permite buscar usuarios de GitHub y ver información
-        detallada sobre ellos.
-      </Text>
-      <Text style={{ marginBottom: 20, textAlign: 'center' }}>
-        Para realizar una búsqueda, simplemente ingresa el nombre de usuario en
-        la pantalla de búsqueda.
-      </Text>
-      <Text>Asegúrate de cumplir con las siguientes validaciones:</Text>
-      <Text>- El texto de búsqueda debe tener al menos 4 caracteres.</Text>
-      <Text>- No se permite buscar la palabra "doublevpartners".</Text>
-      <Button title="Ir a la Búsqueda" onPress={navigateToSearch} />
-    </View>
+      <View style={styles.mainView}>
+        <View style={styles.titleView}>
+          <Text style={styles.titleText}>
+            Bienvenido a la Aplicación GitHub Users
+          </Text>
+        </View>
+
+        <View style={styles.descriptionView}>
+          <Text style={styles.descriptionText}>
+            Esta aplicación te permite buscar usuarios de GitHub y ver
+            información detallada sobre ellos.
+          </Text>
+        </View>
+
+        <StartButton title="Comenzar" onPress={navigateToSearch} />
+      </View>
+    </ImageBackground>
   );
 };
 
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+  },
+
+  mainView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  titleView: {
+    marginTop: 80,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  descriptionView: {
+    marginTop: 200,
+    marginBottom: 80,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  titleText: {
+    textAlign: 'center',
+    fontSize: 32,
+    color: 'white',
+    fontWeight: '500',
+  },
+  descriptionText: {
+    color: '#3498db',
+    fontSize: 28,
+    textAlign: 'center',
+  },
+});
 export default HomeScreen;
